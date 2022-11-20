7576a995831230551880d34f1cce2433b3bdba2d
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2022-11-19 22:25:19 -0500

    Regex validation

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import re
 
 
-def createCharacaters():
+def createCharacters():
     # Generating the alphabet and digits
     alp = [chr(i) for i in range(65, 91)] + [str(i) for i in range(10)]
     # Adding the special characters to the list
     alp.append("_")
-    # print(len(alp))
     return print(alp);
 
 
 def getMessage():
     # Getting the message from the user
-    message = input("Ingrese el mensaje: ")
+    message = input("Ingrese el mensaje, tenga en cuenta que el espacio está representado por '_' : ")
+    if (message == ""):
+        print("No ingresó ningún mensaje")
+        getMessage()
+    if (len(message) > 16):
+        print("El mensaje no puede tener más de 16 caracteres")
+        message = getMessage()
     specialChar = re.compile(r'[^a-zA-Z0-9_]')
-    #Comprobar que el mensaje no contenga caracteres especiales por medio de un Regex
+    # Comprobar que el mensaje no contenga caracteres especiales por medio de un Regex
     if specialChar.search(message) or message == "":
         print("Mensaje inválido intente nuevamente")
         return getMessage()
     else:
-        return print(message.upper())
+
+        return print("El mensaje a cifrar es: "+message.upper())
 
 
+def main():
+    createCharacters()
+    getMessage()
 
-createCharacaters()
-getMessage()
+
+if __name__ == "__main__":
+    main()
